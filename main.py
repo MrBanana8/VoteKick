bf668910838ce530414c0af8c5826463f17a560a
NotBanana8 <gal.loya48@gmail.com>
2024-10-19 09:47:39 +0300

    Initial commit: basic bot functionality

---
+++ main.py
@@ -0,0 +1,78 @@
+import discord
+import os
+from discord.ext import commands
+from discord import app_commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.voice_states = True
+client = commands.Bot(command_prefix='$', intents=intents)
+
+class VoteKickButton(discord.ui.View):
+    def __init__(self, user_to_kick, embed, ctx, voice_members):
+        super().__init__()
+        self.user_to_kick = user_to_kick
+        self.embed = embed
+        self.ctx = ctx
+        self.voice_members = voice_members
+        self.accept_users = []
+        self.declined_users = []
+        self.voted = []
+
+    @discord.ui.button(label="✅ Yes", style=discord.ButtonStyle.green)
+    async def vote_yes(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if interaction.user in self.voted:
+            await interaction.response.send_message(f"You've already voted.", ephemeral=True)
+        else:
+            self.voted.append(interaction.user)
+            self.accept_users.append(interaction.user)
+            self.embed.set_field_at(0, name=f"✅ for YES: {len(self.accept_users)}", value=f"❌ for NO: {len(self.declined_users)}")
+            await interaction.message.edit(embed=self.embed)
+            await interaction.response.defer()
+            await self.check_votes(interaction)
+
+    @discord.ui.button(label="❌ No", style=discord.ButtonStyle.red)
+    async def vote_no(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if interaction.user in self.voted:
+            await interaction.response.send_message(f"You've already voted.", ephemeral=True)
+        else:
+            self.voted.append(interaction.user)
+            self.declined_users.append(interaction.user)
+            self.embed.set_field_at(0, name=f"✅ for YES: {len(self.accept_users)}", value=f"❌ for NO: {len(self.declined_users)}")
+            await interaction.message.edit(embed=self.embed)
+            await interaction.response.defer()
+            await self.check_votes(interaction)
+
+    async def check_votes(self, interaction):
+        if len(self.voted) == len(self.voice_members):
+            if len(self.accept_users) > len(self.declined_users):
+                await self.user_to_kick.move_to(None)
+                await interaction.channel.send(f"{self.user_to_kick.mention} has been disconnected from {self.user_to_kick.voice.channel.mention} by a vote!")
+            else:
+                await interaction.channel.send(f"{self.user_to_kick.mention} will not be disconnected, majority voted no.")
+            self.stop()
+
+@client.tree.command(name="votekick", description="Vote to kick a user from the voice channel")
+async def votekick(interaction: discord.Interaction, user: discord.Member):
+    if user == interaction.user:
+        await interaction.response.send_message("You cannot vote yourself.",ephemeral=True)
+        return
+    if interaction.user.voice is None:
+        await interaction.response.send_message("you are not in a voice channel!", ephemeral=True)
+        return
+    voice_members = interaction.user.voice.channel.members
+
+    embed = discord.Embed(
+        color=discord.Color.purple(),
+        description=f'### Vote by: {interaction.user.mention}\nKick player: {user.mention}'
+    )
+    embed.add_field(name="✅ for YES: 0", value="❌ for NO: 0")
+
+    view = VoteKickButton(user_to_kick=user, embed=embed, ctx=interaction,voice_members=voice_members)
+    await interaction.response.send_message(embed=embed, view=view)
+
+with open("token.0","r",encoding="utf-8") as f:
+    TOKEN = f.read()
+
+client.run(TOKEN)
